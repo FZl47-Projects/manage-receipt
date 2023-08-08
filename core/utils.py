8e3f3f30e2ae640797f897a0790acee3e4890ce2
Fazel Momeni <wwwfazel88@gmail.com>
2023-08-09 02:32:27 +0330

    add func for prefix phonenumber

--- core/utils.py
+++ core/utils.py
@@ -5,6 +5,7 @@ from django.core.mail import send_mail as _send_email_django
 from django.conf import settings
 from django_q.tasks import async_task
 
+
 def random_str(size=15, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
@@ -16,17 +17,22 @@ def get_time(frmt='%Y-%m-%d_%H:%M'):
     return t
 
 
-def send_sms(phonenumber,content,**kwargs):
-    def handle(phonenumber,content,**kwargs):
+def send_sms(phonenumber, content, **kwargs):
+    def handle(phonenumber, content, **kwargs):
         pass
 
     async_task(handle)
 
-def send_email(email,content,**kwargs):
+
+def send_email(email, content, **kwargs):
     # send email in background 
     async_task(_send_email_django,
-        settings.EMAIL_SUBJECT,
-        content,
-        settings.EMAIL_HOST_USER,
-        [email]
-    )
+               settings.EMAIL_SUBJECT,
+               content,
+               settings.EMAIL_HOST_USER,
+               [email]
+               )
+
+
+def add_prefix_phonenum(phonenumber):
+    return f'+98{phonenumber}'
