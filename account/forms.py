6bae8c2d68316b90e8cfc0c92e9d50f29b100a1f
Fazel Momeni <wwwfazel88@gmail.com>
2023-08-09 02:33:27 +0330

    create RegisterUserForm for register user action

--- account/forms.py
+++ account/forms.py
@@ -1,15 +1,32 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-
+from django import forms
 from .models import User
 
-class CustomUserCreationForm(UserCreationForm):
 
+class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('email','phonenumber')
+        fields = ('email', 'phonenumber')
+
 
 class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('email', 'phonenumber')
+
+
+class RegisterUserForm(forms.ModelForm):
+    password2 = forms.CharField(max_length=64, min_length=8, required=True, widget=forms.PasswordInput())
 
     class Meta:
         model = User
-        fields = ('email','phonenumber')
+        fields = ('phonenumber', 'password', 'password2')
+
+    def clean_password2(self):
+        p1 = self.cleaned_data.get('password')
+        p2 = self.cleaned_data.get('password2')
+        if p1 != p2:
+            raise forms.ValidationError('passwords is not same')
+        return p2
+
+
