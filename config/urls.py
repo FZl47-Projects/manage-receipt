7d625fa54f51ad32115a3e24b280d0abb88a3438
FZl-Projects <fzl47.projects@gmail.com>
2023-08-09 01:39:05 +0430

    include urls account

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
+    path('u/',include('account.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+
+
+# 
+from django.http import HttpResponse
+from core.utils import send_email
+
+def test(request):
+    send_email('wwwfazel88@gmail.com','Test email')
+    return HttpResponse('Test')
+
+urlpatterns.append(
+    path('',test)
+)
