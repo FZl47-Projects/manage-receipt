431c489c757729bf59e2e4c652a1d467fe0c4fc2
FZl-Projects <fzl47.projects@gmail.com>
2023-08-09 01:40:55 +0430

    complete send_email with using async_task django-q

--- core/utils.py
+++ core/utils.py
@@ -1,7 +1,9 @@
 import string
 import random
 from django.utils import timezone
-
+from django.core.mail import send_mail as _send_email_django
+from django.conf import settings
+from django_q.tasks import async_task
 
 def random_str(size=15, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
@@ -15,8 +17,16 @@ def get_time(frmt='%Y-%m-%d_%H:%M'):
 
 
 def send_sms(phonenumber,content,**kwargs):
-    pass
+    def handle(phonenumber,content,**kwargs):
+        pass
 
+    async_task(handle)
 
 def send_email(email,content,**kwargs):
-    pass
+    # send email in background 
+    async_task(_send_email_django,
+        settings.EMAIL_SUBJECT,
+        content,
+        settings.EMAIL_HOST_USER,
+        [email]
+    )
