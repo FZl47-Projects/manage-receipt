bcbbe6949d33931ef60cc3d5b0cfd53a576f9ec9
Fazel Momeni <wwwfazel88@gmail.com>
2023-08-09 23:06:23 +0330

    add urls reset password

--- account/urls.py
+++ account/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 app_name = 'account'
 urlpatterns = [
-    path('login-register',views.login_register,name='login_register')
+    path('login-register',views.login_register,name='login_register'),
+    # reset password
+    path('reset-password',views.reset_password,name='reset_password'),
+    path('reset-password/send-code',views.reset_password_send,name='reset_password_send_code'),
+    path('reset-password/check-code',views.reset_password_check,name='reset_password_check_code'),
+    path('reset-password/set',views.reset_password_set,name='reset_password_set'),
 ]
