4db46a1a9dd3c875a9f71aeedeeb654fe3c9ac58
Fazel Momeni <wwwfazel88@gmail.com>
2023-08-09 23:03:24 +0330

    add some function like : send ajax

--- static/scripts/base.js
+++ static/scripts/base.js
@@ -1,3 +1,41 @@
+function redirect(url){
+    window.location.href = url
+}
+
+function sendAjax({url, data, method = 'post', success, error}) {
+
+    success = success || function (response) {
+    }
+    error = error || function (response) {
+        createNotify(
+            {
+                title: 'ارور',
+                message: 'مشکلی در ارسال درخواست وجود دارد لطفا اتصال خود را بررسی کنید',
+                theme: 'error'
+            }
+        )
+    }
+
+    $.ajax(
+        {
+            url: url,
+            data: JSON.stringify(data),
+            type: method,
+            headers: {
+                'X-CSRFToken': window.CSRF_TOKEN
+            },
+            success: function (response) {
+                success(response)
+            },
+            failed: function (response) {
+                error(response)
+            },
+            error: function (response) {
+                error(response)
+            }
+        }
+    )
+}
 
 function getUrlParameter(sParam) {
     var sPageURL = window.location.search.substring(1),
@@ -14,14 +52,14 @@ function getUrlParameter(sParam) {
     return false;
 }
 
-function randomString(length=15) {
+function randomString(length = 15) {
     let result = '';
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     const charactersLength = characters.length;
     let counter = 0;
     while (counter < length) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      counter += 1;
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
     }
     return result;
 }
@@ -50,4 +88,46 @@ function getCookie(name) {
 
 function removeCookie(name) {
     document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+
+function createLoading(element, options = {
+    size: 'normal',
+    color: '#1ee696',
+    fill: null
+
+}) {
+    if (element.classList.contains('loading-element-parent')) {
+        return
+    }
+    let loading = document.createElement('div')
+    loading.className = `loading-element loading-circle-${options.size}`
+    let color = options.color
+    loading.style = `
+        border-top-color:${color};
+        border-right-color:${color};
+    `
+    let loading_blur = document.createElement('div')
+    if (options.fill) {
+        loading_blur.style = `
+            background:${options.fill};
+        `
+        loading_blur.classList.add('fill')
+    }
+    loading_blur.className = 'loading-blur-element'
+    element.append(loading_blur)
+    element.append(loading)
+    element.classList.add('loading-element-parent')
+    element.setAttribute('disabled', 'disabled')
+}
+
+function removeLoading(element) {
+    try {
+        element.querySelector('.loading-element').remove()
+        element.querySelector('.loading-blur-element').remove()
+        element.classList.remove('loading-element-parent')
+        element.removeAttribute('disabled')
+    } catch (e) {
+
+    }
 }
