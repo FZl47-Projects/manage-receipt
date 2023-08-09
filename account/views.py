1e6fb7059ea670301a267a4b8c95711a05bf9c39
Fazel Momeni <wwwfazel88@gmail.com>
2023-08-09 23:06:06 +0330

    reset password completed

--- account/views.py
+++ account/views.py
@@ -1,12 +1,19 @@
+import json
 from django.contrib import messages
+from django.conf import settings
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from core.utils import add_prefix_phonenum
+from django.http import JsonResponse, HttpResponseBadRequest, Http404, HttpResponse
+from django.views.decorators.http import require_POST
+from django.contrib.auth import authenticate, login, get_user_model
+from core.utils import add_prefix_phonenum, random_str
+from core.redis_py import set_value_expire, remove_key, get_value
+from notification.models import NotificationUser
 from . import forms
 
+User = get_user_model()
+RESET_PASSWORD_CONFIG = settings.RESET_PASSWORD_CONFIG
 
 def login_register(request):
-
     def login_perform(request, data):
         phonenumber = data.get('phonenumber', None)
         password = data.get('password', None)
@@ -25,21 +32,25 @@ def login_register(request):
 
     def register_perform(request, data):
         f = forms.RegisterUserForm(data=data)
-        # TODO: should (set validation and message when user is exists) => DONE
-        # TODO: should (check register view)
-        if f.is_bound is False:
+        if f.is_valid() is False:
             messages.error(request, 'لطفا فیلد هارا به درستی وارد نمایید')
             return redirect('account:login_register')
-        if f.is_valid():
-            # create user
-            user = f.save()
-            # login
-            login(request, user)
-            messages.success(request, 'حساب شما با موفقیت ایجاد شد')
-            return redirect('public:home')
-        else:
+        # check for exists user
+        phonenumber = f.cleaned_data['phonenumber']
+        if User.objects.filter(phonenumber=phonenumber).exists():
             messages.error(request, 'کاربری با این شماره از قبل ثبت شده است')
-        return redirect('account:login_register')
+            return redirect('account:login_register')
+        # create user
+        password = f.cleaned_data['password2']
+        user = User(
+            phonenumber=phonenumber,
+        )
+        user.set_password(password)
+        user.save()
+        # login
+        login(request, user)
+        messages.success(request, 'حساب شما با موفقیت ایجاد شد')
+        return redirect('public:home')
 
     if request.method == 'GET':
         return render(request, 'account/login-register.html')
@@ -54,4 +65,103 @@ def login_register(request):
 
 
 def reset_password(request):
-    pass
+    return render(request, 'account/reset-password.html')
+
+
+@require_POST
+def reset_password_send(request):
+    # AJAX view
+    data = json.loads(request.body)
+    phonenumber = data.get('phonenumber', None)
+    # validate data
+    if not phonenumber:
+        return HttpResponseBadRequest()
+    # check user is exists
+    try:
+        phonenumber = add_prefix_phonenum(phonenumber)
+        user = User.objects.get(phonenumber=phonenumber)
+    except:
+        raise Http404
+    code = random_str(RESET_PASSWORD_CONFIG['CODE_LENGTH'])
+    key = RESET_PASSWORD_CONFIG['STORE_BY'].format(phonenumber)
+    # check code state set
+    if get_value(key) is not None:
+        # code is already set
+        return HttpResponse(status=409)
+    # set code
+    set_value_expire(key,code,RESET_PASSWORD_CONFIG['TIMEOUT'])
+    # send code
+    NotificationUser.objects.create(
+        to_user=user,
+        title='بازیابی رمز عبور',
+        description=f"""
+             کد بازیابی رمز عبور : 
+             {code}
+        """,
+        send_notify=True
+    )
+    return JsonResponse({})
+
+
+@require_POST
+def reset_password_check(request):
+    # AJAX view
+    data = json.loads(request.body)
+    phonenumber = data.get('phonenumber', None)
+    code = data.get('code', None)
+    # validate data
+    if (not code) or (not phonenumber):
+        return HttpResponseBadRequest()
+    phonenumber = add_prefix_phonenum(phonenumber)
+    key = RESET_PASSWORD_CONFIG['STORE_BY'].format(phonenumber)
+    # check code
+    code_stored = get_value(key)
+    if code_stored is None:
+        # code is not seted or timeout
+        return HttpResponse(status=410)
+    if code_stored != code:
+        # code is wrong(not same)
+        return HttpResponse(status=409)
+    return JsonResponse({})
+
+
+@require_POST
+def reset_password_set(request):
+    # AJAX view
+    data = json.loads(request.body)
+    f = forms.ResetPasswordSet(data)
+    # validate data
+    if f.is_valid() is False:
+        return HttpResponseBadRequest()
+    clean_data = f.cleaned_data
+    # phonenumber must get from data (not clean_data)
+    phonenumber = data['phonenumber']
+    code = clean_data['code']
+    password = clean_data['password2']
+    # check user is exists
+    try:
+        phonenumber = add_prefix_phonenum(phonenumber)
+        user = User.objects.get(phonenumber=phonenumber)
+    except:
+        raise Http404
+    key = RESET_PASSWORD_CONFIG['STORE_BY'].format(phonenumber)
+    # check code
+    code_stored = get_value(key)
+    if code_stored is None:
+        # code is not seted or timeout
+        return HttpResponse(status=410)
+    if code_stored != code:
+        # code is wrong(not same)
+        return HttpResponse(status=409)
+    user.set_password(password)
+    user.save()
+    remove_key(key)
+    NotificationUser.objects.create(
+        to_user=user,
+        title='رمز عبور شما تغییر کرد',
+        description="""
+            رمز عبور شما با موفقیت تغییر کرد
+        """,
+        send_notify=True
+    )
+    return JsonResponse({})
