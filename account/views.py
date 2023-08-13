c3a889816ef431db6aceea4cecd75dcd26239056
FZl-Projects <fzl47.projects@gmail.com>
2023-08-13 15:50:02 +0430

    add ticket list new and ticket add

--- account/views.py
+++ account/views.py
@@ -16,6 +16,8 @@ from receipt.forms import BuildingForm
 from receipt.models import Building
 from notification.forms import NotificationForm, NotificationUserForm
 from notification.models import Notification, NotificationUser
+from support.models import Ticket
+from support.forms import TicketForm
 
 from . import forms
 
@@ -392,4 +394,52 @@ class NotificationUserList(View):
 
     @admin_required_cbv()
     def post(self, request):
+        pass
+
+
+class TicketAdd(View):
+    template_name = 'account/dashboard/base/support/ticket/add.html'
+
+    @admin_required_cbv()
+    def get(self,request):
+        context = {
+            'degrees_of_importance': Ticket.DEGREE_OF_IMPORTANCE_OPTIONS,
+            'users': User.objects.all()
+        }
+        return render(request,self.template_name,context)
+    
+    @admin_required_cbv()
+    def post(self,request):
+        data = request.POST.copy()
+        # add this admin for who created ticket
+        data['from_user'] = request.user
+        f = TicketForm(data,request.FILES)
+        if form_validate_err(request,f) is False:
+            return render(request,self.template_name)
+        f.save()
+        messages.success(request,'تیکت پشتیبانی با موفقیت ایجاد شد')
+        return redirect('account:support_ticket_add')
+
+
+class TicketListNew(View):
+    template_name = 'account/dashboard/base/support/ticket/list-new.html'
+
+    @admin_required_cbv()
+    def get(self,request):
+        page_num = request.GET.get('page', 1)
+        tickets = Ticket.objects.filter(is_open=True)
+        pagination = Paginator(tickets, 40)
+        pagination = pagination.get_page(page_num)
+        tickets = pagination.object_list
+        # order tickets
+        order_by = ['high','medium','low']
+        tickets = sorted(tickets, key=lambda x: order_by.index(x.degree_of_importance))
+        context = {
+            'tickets': tickets,
+            'pagination': pagination
+        }
+        return render(request,self.template_name,context)
+    
+    @admin_required_cbv()
+    def post(self,request):
         pass
