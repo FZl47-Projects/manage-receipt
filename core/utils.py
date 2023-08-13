9b60aab4914e16ab9b31fefe96cc48185fc77736
FZl-Projects <fzl47.projects@gmail.com>
2023-08-13 15:51:09 +0430

    add get_timesince_persian in utils core

--- core/utils.py
+++ core/utils.py
@@ -1,5 +1,6 @@
 import string
 import random
+import datetime
 from django.utils import timezone
 from django.core.mail import send_mail as _send_email_django
 from django.conf import settings
@@ -16,6 +17,33 @@ def get_time(frmt='%Y-%m-%d_%H:%M'):
     if frmt is not None:
         t = t.strftime(frmt)
     return t
+
+def get_timesince_persian(time):
+    time_server = get_time(None)
+
+    diff_time = datetime.datetime(time_server.year, time_server.month, time_server.day, time_server.hour,
+                                       time_server.minute) - datetime.datetime(time.year, time.month, time.day, time.hour,
+                                                                            time.minute)
+
+    diff_time_sec = diff_time.total_seconds()
+    # sec = diff_time_sec % 60
+    min = int(diff_time_sec // 60 % 60)
+    hour = int(diff_time_sec // 3600)
+    day = diff_time.days
+    result = ''
+    if min > 0:
+        result = f'{min} دقیقه پیش'
+    else:
+        result = f'لحظاتی پیش'
+
+    if hour > 0:
+        result = f'{hour} ساعت پیش'
+
+    if day > 0:
+        result = f'{day}  روز پیش'
+
+    return result
+
 
 
 def send_sms(phonenumber, content, **kwargs):
