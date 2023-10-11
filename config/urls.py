d832c5f2fdc3651d06505191bbbe9511e4664051
Fazel Momeni <wwwfazel88@gmail.com>
2023-10-11 13:24:31 +0330

    fix bug str cant join paths(production bug|in production url media_root is string)

--- config/urls.py
+++ config/urls.py
@@ -18,22 +18,19 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
-    path('',include('public.urls',namespace='public')),
-    path('',include('receipt.urls',namespace='receipt')),
-    path('',include('notification.urls',namespace='notification')),
-    path('',include('support.urls',namespace='support')),
-    path('u/',include('account.urls',namespace='account')),
+    path('', include('public.urls', namespace='public')),
+    path('', include('receipt.urls', namespace='receipt')),
+    path('', include('notification.urls', namespace='notification')),
+    path('', include('support.urls', namespace='support')),
+    path('u/', include('account.urls', namespace='account')),
     path('anything-except-admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
-
-
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler403 = 'public.views.err_403_handler'
 handler404 = 'public.views.err_404_handler'
-handler500  = 'public.views.err_500_handler'
+handler500 = 'public.views.err_500_handler'
